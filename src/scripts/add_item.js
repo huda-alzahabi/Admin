ef74f24e01eb042dcc573b4dee073baa235c7f0c
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-13 00:06:22 +0300

    Fixed  add item method with image for each item

--- src/scripts/add_item.js
+++ src/scripts/add_item.js
@@ -1,27 +1,25 @@
 function addItem() {
-    const item_image = document.getElementById("inputTag");
+    let base64String = "";
+
+    const addBtn = document.getElementById("add_items");
+    addBtn.addEventListener("click", newItem);
+    const item_image = document.getElementById("it_image");
+    item_image.addEventListener("change", getImage);
+
     const item_name = document.getElementById("it_name");
     const item_price = document.getElementById("it_price");
     const item_cat = document.getElementById("it_cat");
-    const addBtn = document.getElementById("add_items");
-    addBtn.addEventListener("click", newItem);
 
     function newItem() {
         let data = new FormData();
         data.append("name", item_name.value);
         data.append("price", item_price.value);
-        data.append(
-            "image",
-            document.querySelector("input[type=file]")["files"][0]
-        );
+        data.append("image", base64String);
         data.append("categories_id", item_cat.value);
         axios({
             method: "post",
             url: "http://127.0.0.1:8000/api/v1/admin/add_item",
             data: data,
-            headers: {
-                "Content-Type": "multipart/form-data",
-            },
         }).then(function(response) {
             item_name.value = "";
             item_price.value = "";
@@ -32,6 +30,16 @@ function addItem() {
                 alert("Item Added!");
             }
         });
+    }
+
+    function getImage() {
+        var file = document.querySelector("input[type=file]")["files"][0];
+        var reader = new FileReader();
+        reader.onload = function() {
+            base64String = reader.result.replace("data:", "").replace(/^.+,/, "");
+            imageBase64Stringsep = base64String;
+        };
+        reader.readAsDataURL(file);
     }
 }
 addItem();
