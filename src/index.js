c9a8b2a08d0d63ba3dbadb82677e073a34d49485
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-12 09:42:55 +0300

    Deleted signup

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const createWindow = () => {
     });
 
     // and load the index.html of the app.
-    mainWindow.loadFile(path.join(__dirname, "login_signup.html"));
+    mainWindow.loadFile(path.join(__dirname, "login.html"));
 
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
