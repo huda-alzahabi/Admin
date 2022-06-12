2147a9079bbe866a26b6e41225928e61f2d7d666
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-12 13:16:12 +0300

    Implemented add category functionality

---
+++ src/scripts/add_category.js
@@ -0,0 +1,23 @@
+function addCategory() {
+    let add_category = document.getElementById("view_categories");
+    add_category.addEventListener("click", function(e) {
+        e.preventDefault();
+        let name = document.getElementById("cat").value;
+        let data = new FormData();
+        data.append("name", name);
+
+        axios({
+            method: "post",
+            url: "http://127.0.0.1:8000/api/v1/admin/add_category",
+            data: data,
+        }).then(function(response) {
+            document.getElementById("cat").value = "";
+            let result = response.data;
+            let message = result.status;
+            if (message == "Success") {
+                alert("Category Added!");
+            }
+        });
+    });
+}
+addCategory();
